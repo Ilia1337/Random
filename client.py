5d0f2a5a10d6c7767966529c88d0491fb22dddfa
ilia1337 <iliafedosenko17@gmail.com>
2023-10-12 17:12:59 +0300

    Code managment complete .gitignore edited

--- client.py
+++ client.py
@@ -1,22 +1,34 @@
+# импорты
 import socket
 import time
 import pygame
 
+# высота окна pygame
+WIDTH = 800 
+HEIGHT = 600
 
+# сокеты 
 m2_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 m2_sock.setsockopt(socket.IPPROTO_TCP,socket.TCP_NODELAY,1)
 m2_sock.connect(("localhost",10000)) 
 
-
+# инициирование pygame
 pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Бактерия")
 
+# цикл крестика
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
 
-
-
-
-
-
-while True:
+# отправка сообщение на сервер
     m2_sock.send("Кто жил и мыслил, тот не может в душе не презирать людей".encode())
     print("Файл отослан")
-    time.sleep(0.5)
+    data = m2_sock.recv(1024).decode()    
+    print(f"Получено {data}") 
+
+# выход из pygame
+pygame.quit()
--- server.py
+++ server.py
@@ -1,19 +1,23 @@
+# импорты
 import time
 import socket
 
+# создание сокетов
 main_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 main_sock.setsockopt(socket.IPPROTO_TCP,socket.TCP_NODELAY,1)
 
-
+# слушание клиента
 main_sock.bind(("localhost",10000))     
 main_sock.setblocking(False)        
 main_sock.listen(5)
 print("Сокет создан")
 
+# цикл соединения с клиентом
 list_us = []
 while True:
     try:
         new_sock,addr = main_sock.accept()
+        print(list_us)
         print("Подключился!",addr)
         new_sock.setblocking(False)                 
         list_us.append(new_sock)
@@ -26,4 +30,12 @@ while True:
                 
             except:
                 pass
+    for sock in list_us:
+        try:
+            sock.send("Игра".encode())
+        except:
+            list_us.remove(sock)
+            sock.close()
+            print("Сокет закрыт")
     time.sleep(0.1)
+    
