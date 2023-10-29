465576cc385e286536c1294bf9bab7e1e3d99ae2
ilia1337 <iliafedosenko17@gmail.com>
2023-10-29 09:53:18 +0300

    добавленны новые комментарии

--- client.py
+++ client.py
@@ -2,34 +2,47 @@
 import socket
 import time
 import pygame
+import math
 
-# высота окна pygame
+# константы (WIDTH - высота, HEIGHT - ширина, CS - Центр экрана)
 WIDTH = 800 
 HEIGHT = 600
 CS = (WIDTH//2,HEIGHT//2)
 
+# помощь коду 
 old_vector = (0,0)
 radius = 50 
 
-# сокеты
+# сокеты (AF_INET - используется семейства ipv4, SOCK_STREAM - используется протокол TCP, TCP_NODELAY - отключение задержки TCP, 10000 - порт)
 m2_sock= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 m2_sock.setsockopt(socket.IPPROTO_TCP,socket.TCP_NODELAY,1)
 m2_sock.connect(("localhost",10000)) 
 
-# инициирование pygame
+# иницилизация pygame и экрана (setmode - задание разрешения, setcaption - названия окна)
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Бактерия")
 
-# цикл крестика
+# цикл закрытия (используется через run), вектора (getfocused, lenght - длинна)
 run = True
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        
+        # создание вектора 
         if pygame.mouse.get_focused():
             pos = pygame.mouse.get_pos()
             vector = pos[0]-CS[1],pos[1]-CS[1]
+            length =  math.sqrt(vector[0]**2+vector[1]**2)
+            vector = vector[0]/length, vector[1]/length                         
+
+            # радиуса
+            print(vector)         
+            if length <= radius:
+                vector = 0,0
+
+            # сравнение с old.vector(0, 0)
             if vector != old_vector:
                 old_vector = vector
                 msg = f'<{vector[0]},{vector[1]}>'
@@ -37,19 +50,18 @@ while run:
 
 
                             
-# отправка сообщение на сервер
+# отправка сообщение на сервер (recv - количество байт кодировки, decode - декодировать сообзение)
     # m2_sock.send("Кто жил и мыслил, тот не может в душе не презирать людей".encode())
     print("Файл отослан")
     data = m2_sock.recv(1024).decode()    
     print(f"Получено {data}") 
     
     
-    
+    # заливка фона окна и радиус круга 
     screen.fill("gray")
     pygame.draw.circle(screen,(255,0,0),CS,radius)
     pygame.display.update()
-    #if vector                                        
-
+    # if vector:                                       
 
 # выход из pygame
 pygame.quit()
--- server.py
+++ server.py
@@ -1,41 +1,124 @@
-# импорты
+# импорт модюля time и socket
 import time
 import socket
+import psycopg2
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base, sessionmaker
+import pygame
 
-# создание сокетов
+pygame.init()
+
+WIDTH_ROOM,HEIGHT_ROOM = 4000,4000
+WIDTH_SERVER,HEIGHT_SERVER = 300,300 
+FPS = 60
+
+
+
+# создание сокетов (AF_INET - используется семейства ipv4, SOCK_STREAM - используется протокол TCP, TCP_NODELAY - отключение задержки TCP)
 main_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 main_sock.setsockopt(socket.IPPROTO_TCP,socket.TCP_NODELAY,1)
 
-# слушание клиента
+# настройка сервера (local host - сервер на компе, 1000 - порт, SETBLOCKING - отключение выключения сервера при заканчиваннии обработки 1 клиента, listen - количество прослушиваемых клиентов.)
 main_sock.bind(("localhost",10000))     
 main_sock.setblocking(False)        
 main_sock.listen(5)
 print("Сокет создан")
 
-# цикл соединения с клиентом
-list_us = []
+# создание соеденения с pgadmin4 (в будущем engin)
+engine = create_engine('postgresql+psycopg2://postgres:papazhiv@localhost/postgres')
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
+s = Session()
+
+# создание окна (название,задание разрешения)
+screen = pygame.display.set_mode((WIDTH_SERVER,HEIGHT_SERVER))
+pygame.display.set_caption("СЕРВЕР")
+
+# фпс
+clock = pygame.time.Clock()
+
+# создаём класс игрока и неаследуемся от базы 
+class Player(Base):
+    __tablename__ = 'gamers' 
+    id = Column(Integer,primary_key=True,autoincrement=True)
+    name = Column(String(250))
+    adres = Column(String)
+    x = Column(Integer, default=500)
+    y = Column(Integer, default=500)
+    size = Column(Integer, default=50)
+    errors = Column(Integer,default= 0)
+    abs_speed = Column(Integer, default=1)
+    speed_x = Column(Integer, default=0)
+    speed_y = Column(Integer, default=0)
+    
+    # иницилизируем класс
+    def __init__(self,name,adres):
+        self.name = name
+        self.adres = adres
+                                    
+# создаём класс локального игрока
+class Local_player:
+    def __init__(self,id,name,sock,adres):
+        self.id = id
+        self.db: Player = s.get(Player,self.id)
+        self.name = name
+        self.sock = sock
+        self.adres = adres
+        self.x = 500
+        self.y = 500
+        self.size = 50 
+        self.errors = 0
+        self.abs_speed = 1 
+        self.speed_x = 0
+        self.speed_y = 0
+
+
+Base.metadata.create_all(engine)
+
+# цикл соединения с клиентом (recv - количество байт кодировки, decode - декодироват ь сообзение)
+list_us = {}
 while True:
+    clock.tick(FPS)
+    
+    # принятие игрока
     try:
-        new_sock,addr = main_sock.accept()
-        print(list_us)
+        new_sock,addr = main_sock.accept()                                         
         print("Подключился!",addr)
         new_sock.setblocking(False)                 
-        list_us.append(new_sock)
+        
+        # получаем адресс и сохжраняем данные о нём ы дб
+        player = Player("Имя",addr)
+        s.merge(player)
+        s.commit()
+    
+        # фильтруем по адресу и ищем игрока
+        addr = f'({addr[0]},{addr[1]})'
+        data = s.query(Player).filter(Player.adres == addr)
+        
+        # проверяем айди
+        for user in data:
+            player = Local_player(user.id, "Имя", new_sock,addr)
+            list_us[user.id] = player
+        
+        # получение данных
     except BlockingIOError:
-        pass      
-    for i in list_us:
+        pass   
+        
+        for id in list(list_us):
             try:
-                data = i.recv(1024).decode()
-                print(f"Я получил {data}!")
-                
+                data = list_us[id].sock.recv(1024).decode()
+                print("Получил", data)
             except:
                 pass
-    for sock in list_us:
-        try:
-            sock.send("Игра".encode())
-        except:
-            list_us.remove(sock)
-            sock.close()
-            print("Сокет закрыт")
-    time.sleep(0.1)
-    
+        
+        # проверяем не вышел ли игрок и закрываем сервер
+        for id in list(list_us):
+            try:
+                list_us[id].sock.send("Игра".encode())
+            except:
+                list_us[id].sock.close()
+                del list_us[id]            
+                s.query(Player).filter(Player.id == id).delete()
+                s.commit()
+                print("Сокет закрыт")    
